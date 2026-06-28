b8e2a6e6a3e98ceb8f06fea301c29d17374d6c47
ankitsejwal <ankit@sejw.al>
2026-06-29 01:36:58 +0530

    Add shadcn Select/Switch/Slider/Tooltip primitives

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,10 +1,15 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { useSessionPersistence } from '@/hooks/use-session-persistence'
 
 function RootLayout() {
   useSessionPersistence()
-  return <Outlet />
+  return (
+    <TooltipProvider delayDuration={300}>
+      <Outlet />
+    </TooltipProvider>
+  )
 }
 
 export const Route = createRootRoute({
